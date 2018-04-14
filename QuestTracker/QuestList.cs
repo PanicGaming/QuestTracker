cbf9d4f88fef42ae72a54cfbd0e16483fc93e3e8
Robert L Quigley <lexm@pobox.com>
2018-04-14 15:09:04 -0400

    Add quest list class

---
+++ QuestTracker/QuestList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuestTracker
+{
+    class QuestList
+    {
+        public QuestList()
+        {
+            quests = new List<Quest>();
+        }
+
+        private List<Quest> quests;
+        public List<Quest> Quests => quests;
+        
+    }
+}
